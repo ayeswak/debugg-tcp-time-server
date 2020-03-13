d3beeb2fff6438023cf4245d5bb731bb7602529a
ayeswak <62135339+ayeswak@users.noreply.github.com>
2020-03-13 10:28:58 +0100

    Update time-server.js

--- time-server.js
+++ time-server.js
@@ -2,6 +2,6 @@ const net = require('net')
 const server = net.createServer(function (socket) {
   // socket handling logic
   let date = new Date();
-  socket.end(`{date.getFullYear()}-{date.getMonth()+1}-{date.getDate()} {date.getHours()}:{date.getMinutes()}\n`);
+  socket.end(`{date.getFullYear()}-{date.getMonth()+1}-{date.getDate()} {date.getHours()}:{date.getMinutes()}\r\n`);
 })
 server.listen(31337)
